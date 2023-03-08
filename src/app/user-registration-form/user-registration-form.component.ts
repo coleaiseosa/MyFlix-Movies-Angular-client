c8389d33fff29b78178c19f1e5e54f94c2411975
coleaiseosa <coleaiseosa@gmail.com>
2023-03-08 14:34:31 +0100

    implemented logic for login and registration

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// to import the APi
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// to display notifications to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit { 
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  // This function is responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // this will close the modal on success
+      this.dialogRef.close();
+      console.log(Response);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
