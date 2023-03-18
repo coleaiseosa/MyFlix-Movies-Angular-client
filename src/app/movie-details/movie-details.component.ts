23d9bb7027bc8b0c1540f1b7b73240dc4cc55f05
coleaiseosa <coleaiseosa@gmail.com>
2023-03-18 17:04:14 +0100

    Added movie-details component

---
+++ src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Description: string;
+    }
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+}
