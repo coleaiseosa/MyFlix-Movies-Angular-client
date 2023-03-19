45458cc0475ed1baa01abea6ca5943c5fc8b47fe
coleaiseosa <coleaiseosa@gmail.com>
2023-03-19 18:13:12 +0100

    fixed movie link bug

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 // // Declares the api url that will provide data for the client app
-const apiUrl = 'https://marcey-flix-movie-app.herokuapp.com/';
+const apiUrl = 'https://marcey-flix-movie-app.herokuapp.com';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,21 +16,21 @@ export class FetchApiDataService {
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
-    .post(apiUrl + 'users', userDetails)
+    .post(apiUrl + '/users', userDetails)
     .pipe(catchError(this.handleError));
   };
 
   public userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
-    .post(apiUrl + 'login', userDetails)
+    .post(apiUrl + '/login', userDetails)
     .pipe(catchError(this.handleError));
   }
 
   getAllMovies(): Observable<any> {
   const token = localStorage.getItem('token');
   return this.http
-    .get(apiUrl + 'movies', {
+    .get(apiUrl + '/movies', {
       headers: new HttpHeaders({
         Authorization: 'Bearer ' + token,
       }),
